9e1b7467a1ae06df8ed8de0054defd7427b7aa44
Oscar Carvajal <os.carvajalmora@gmail.com>
2025-02-02 01:24:58 -0300

    chore: solution code refactor

--- src/solutions.ts
+++ src/solutions.ts
@@ -8,68 +8,42 @@ export const calculatePanelsInARectangleRoof = ({
   roofHeight, // Y
 }: CalculatePanelsInARectangleRoofProps): number => {
   if (!panelWidth || !panelHeight || !roofWidth || !roofHeight) {
-    throw new Error("All parameters are required.");
+    throw new Error("Missing required parameters.");
   }
 
-  const maxPanelsAxBPosition =
+  // Step 1: Calculate the number of panels in the neutral position (AxB)
+  let neutralPositionPanelsQuantity =
     Math.floor(roofWidth / panelWidth) * Math.floor(roofHeight / panelHeight);
 
-  const maxPanelsBxAPosition =
-    Math.floor(roofWidth / panelHeight) * Math.floor(roofHeight / panelWidth);
+  let rotatedRemainingSpacePanelsQuantity = 0;
 
-  const maxPanelQuantity = Math.max(maxPanelsAxBPosition, maxPanelsBxAPosition);
+  // Step 2: Check if there any remaining space
+  const hasHorizontalRemainingSpace = hasRemainingSpace(roofWidth, panelWidth);
+  const hasVerticalRemainingSpace = hasRemainingSpace(roofHeight, panelHeight);
 
-  if (maxPanelQuantity === 0) {
-    return 0;
+  // Step 3: If there is remaining horizontal space, calculate the panels in a rotated position
+  if (hasHorizontalRemainingSpace) {
+    // Note: I treat the remaining space as the measurements of a new "rotated" roof. So I can use recursion
+    rotatedRemainingSpacePanelsQuantity = calculatePanelsInARectangleRoof({
+      panelWidth,
+      panelHeight,
+      roofWidth: roofHeight,
+      roofHeight: roofWidth % panelWidth,
+    });
   }
 
-  let panelsQuantity = maxPanelQuantity;
-
-  if (maxPanelQuantity === maxPanelsAxBPosition) {
-    const hasHorizontalSpace = hasRemainingSpace(roofWidth, panelWidth);
-    const hasVerticalSpace = hasRemainingSpace(roofHeight, panelHeight);
-
-    if (hasHorizontalSpace) {
-      panelsQuantity += calculatePanelsInARectangleRoof({
-        panelWidth,
-        panelHeight,
-        roofWidth: roofWidth % panelWidth,
-        roofHeight,
-      });
-    }
-
-    if (hasVerticalSpace) {
-      panelsQuantity += calculatePanelsInARectangleRoof({
-        panelWidth,
-        panelHeight,
-        roofWidth,
-        roofHeight: roofHeight % panelHeight,
-      });
-    }
-  } else {
-    const hasHorizontalSpace = hasRemainingSpace(roofWidth, panelHeight);
-    const hasVerticalSpace = hasRemainingSpace(roofHeight, panelWidth);
-
-    if (hasHorizontalSpace) {
-      panelsQuantity += calculatePanelsInARectangleRoof({
-        panelWidth,
-        panelHeight,
-        roofWidth: roofWidth % panelHeight,
-        roofHeight,
-      });
-    }
-
-    if (hasVerticalSpace) {
-      panelsQuantity += calculatePanelsInARectangleRoof({
-        panelWidth,
-        panelHeight,
-        roofWidth,
-        roofHeight: roofHeight % panelWidth,
-      });
-    }
+  // Step 4: Same as step 3, but now treating the heigh
+  if (hasVerticalRemainingSpace) {
+    rotatedRemainingSpacePanelsQuantity = calculatePanelsInARectangleRoof({
+      panelWidth,
+      panelHeight,
+      roofWidth: roofHeight % panelHeight,
+      roofHeight: roofWidth,
+    });
   }
 
-  return panelsQuantity;
+  // Step 5: Return the total number of panels (neutral + rotated)
+  return neutralPositionPanelsQuantity + rotatedRemainingSpacePanelsQuantity;
 };
 
 export const calculatePanelsInATriangleRoof = () => {
